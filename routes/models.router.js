a8e11d13e08472b4ab4878b46616f0b971c74a9e
Camilovv25 <115130294+Camilovv25@users.noreply.github.com>
2023-03-23 06:40:30 -0500

    feat: adding services, controllers and routes of States endpoint

--- routes/models.router.js
+++ routes/models.router.js
@@ -1,12 +1,13 @@
 
 const router = require('express').Router();
-const routesUsers = require('./users.router')
-const routesAuth = require('./auth.routes')
-const roles = require('./roles.router')
-const passport = require('../libs/passport')
+const routesUsers = require('./users.router');
+const routesAuth = require('./auth.routes');
+const roles = require('./roles.router');
+const passport = require('../libs/passport');
 const routesPublicationsTypes = require('./publicationsTypes.routes');
 const routesCountries = require ( './countries.routes');
-const routesTags = require('./tags.routes')
+const routesTags = require('./tags.routes');
+const routesStates = require('./states.routes');
 
 // const isAuthenticatedByPassportJwt = require('../libs/passport')
 
@@ -17,14 +18,13 @@ function routerModels(app) {
   router.use('/publications-types', routesPublicationsTypes);
   router.use('/countries', routesCountries);
   router.use('/tags', routesTags);
+  router.use('/states', routesStates);
 
   router.use('/users',passport.authenticate('jwt', { session: false }), routesUsers);
 
   router.use('/roles', roles);
 
-
-
-
+}
 module.exports = routerModels;
 
 
